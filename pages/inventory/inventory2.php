c80e7208339452698c89973c3ceb2837c0b6951b
Joseph Emmanuel Balmes <joembalmes06@gmail.com>
2024-07-30 12:07:48 +0800

    .

--- pages/inventory/inventory2.php
+++ pages/inventory/inventory2.php
@@ -99,71 +99,104 @@ include ("../../phpscripts/check-login.php");
     </nav>
 
     <section class="home">
-    <header class="contractheader">
-        <div class="container-header">
-            <h1 class="title">Inventory</h1>
+        <header class="contractheader">
+            <div class="container-header">
+                <h1 class="title">Inventory</h1>
+            </div>
+        </header>
+        <div class="filter-container">
+            <div class="filters">
+                <input type="text" placeholder="Search" id="search">
+                <input type="date" id="date">
+                <select id="franchise">
+                    <option value="">Franchise</option>
+                    <option value="potato_corner">Potato Corner</option>
+                    <option value="auntie_annes">Auntie Anne's</option>
+                    <option value="macao">Macao Imperial Tea</option>
+                </select>
+                <button id="new-report" data-bs-toggle="modal" data-bs-target="#modalReport">New Report</button>
+            </div>
         </div>
-    </header>
-    <div class="filter-container">
-        <div class="filters">
-            <input type="text" placeholder="Search" id="search">
-            <input type="date" id="date">
-            <select id="franchise">
-                <option value="">Franchise</option>
-                <option value="potato_corner">Potato Corner</option>
-                <option value="auntie_annes">Auntie Anne's</option>
-                <option value="macao">Macao Imperial Tea</option>
-            </select>
-            <button id="new-report">New Report</button>
+
+        <div class="container">
+            <table class="inventory-table">
+                <thead>
+                    <tr>
+                        <th>Franchisee</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr data-franchisee="Potato Corner" data-location="Lyceum of the Philippines" data-date="5/20/24">
+                        <td><img src="../../assets/images/PotCor.png" alt="Potato Corner"> Lyceum of the Philippines
+                        </td>
+                        <td>Lyceum of the Philippines</td>
+                        <td>5/20/24</td>
+                    </tr>
+                    <tr data-franchisee="Auntie Anne's" data-location="Urdaneta, Pangasinan" data-date="5/20/24">
+                        <td><img src="../../assets/images/AuntieAnn.png" alt="Auntie Anne's"> Urdaneta, Pangasinan</td>
+                        <td>Urdaneta, Pangasinan</td>
+                        <td>5/20/24</td>
+                    </tr>
+                    <tr data-franchisee="Macao Imperial Tea" data-location="SM Grand Central" data-date="5/20/24">
+                        <td><img src="../../assets/images/MacaoImp.png" alt="Macao Imperial Tea"> SM Grand Central</td>
+                        <td>SM Grand Central</td>
+                        <td>5/20/24</td>
+                    </tr>
+                    <!-- Add more rows as needed -->
+                </tbody>
+            </table>
         </div>
-    </div>
 
-    <div class="container">
-        <table class="inventory-table">
-            <thead>
-                <tr>
-                    <th>Franchisee</th>
-                    <th>Location</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr data-franchisee="Potato Corner" data-location="Lyceum of the Philippines" data-date="5/20/24">
-                    <td><img src="../../assets/images/PotCor.png" alt="Potato Corner"> Lyceum of the Philippines</td>
-                    <td>Lyceum of the Philippines</td>
-                    <td>5/20/24</td>
-                </tr>
-                <tr data-franchisee="Auntie Anne's" data-location="Urdaneta, Pangasinan" data-date="5/20/24">
-                    <td><img src="../../assets/images/AuntieAnn.png" alt="Auntie Anne's"> Urdaneta, Pangasinan</td>
-                    <td>Urdaneta, Pangasinan</td>
-                    <td>5/20/24</td>
-                </tr>
-                <tr data-franchisee="Macao Imperial Tea" data-location="SM Grand Central" data-date="5/20/24">
-                    <td><img src="../../assets/images/MacaoImp.png" alt="Macao Imperial Tea"> SM Grand Central</td>
-                    <td>SM Grand Central</td>
-                    <td>5/20/24</td>
-                </tr>
-                <!-- Add more rows as needed -->
-            </tbody>
-        </table>
-    </div>
-
-    <div id="notification-area">
-        <h2>Stock Notification</h2>
-        <ul id="notification-list">
-            <li>
-                <h3>5/20/24</h3>
-                <h4>Potato Corner</h4>
-                <h4>Circuit Makati</h4>
-                <span class="notification-details">Less than threshold amount: SOFTDRINKS IN CAN - COKE</span>
-                <button>Resolve</button>
-            </li>
-        </ul>
-    </div>
-
-    <!-- content -->
+        <div id="notification-area">
+            <h2>Stock Notification</h2>
+            <ul id="notification-list">
+                <li>
+                    <h3>5/20/24</h3>
+                    <h4>Potato Corner</h4>
+                    <h4>Circuit Makati</h4>
+                    <span class="notification-details">Less than threshold amount: SOFTDRINKS IN CAN - COKE</span>
+                    <button>Resolve</button>
+                </li>
+            </ul>
+        </div>
+        <!-- content -->
     </section>
-    
+    <!-- Modal -->
+    <div class="modal fade" id="modalReport" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-0 text-light fw-bold" style="background-color: #1f375d">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">New Inventory Report</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row flex-column gap-4">
+                        <div class="col text-center">
+                            <button class="btn bg-secondary-subtle w-100 select-franchisee-report"
+                                data-franchisee="potato-corner">
+                                <img src="../../assets/images/PotCor.png" height="100%" width="150px" alt="img">
+                            </button>
+                        </div>
+                        <div class="col text-center">
+                            <button class="btn bg-secondary-subtle w-100 select-franchisee-report"
+                                data-franchisee="auntie-anne">
+                                <img src="../../assets/images/AuntieAnn.png" height="100%" width="150px" alt="img">
+                            </button>
+                        </div>
+                        <div class="col text-center">
+                            <button class="btn bg-secondary-subtle w-100 select-franchisee-report"
+                                data-franchisee="macao-imperial">
+                                <img src="../../assets/images/MacaoImp.png" height="100%" width="150px" alt="img">
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <!-- JS -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"
@@ -175,7 +208,7 @@ include ("../../phpscripts/check-login.php");
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
     <script src="../../assets/js/navbar.js"></script>
-    <!-- <script src="../../assets/js/content.js"></script> -->
+    <script src="../../assets/js/report-script.js"></script>
 </body>
 
 </html>
--- pages/manpower/selectActiveBranch.php
+++ pages/manpower/selectActiveBranch.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+
+include ("../../phpscripts/database-connection.php");
+include ("../../phpscripts/check-login.php");
+?>
 <!DOCTYPE html>
 
 <html lang="en">
